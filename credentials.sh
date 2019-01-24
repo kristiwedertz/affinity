123c92e485be5e77af91ee332346fba141db6b17
Lisa Smith <lisa.smith@metalab.csun.edu>
2019-01-23 16:27:18 -0800

    Testing docker build

--- credentials.sh
+++ credentials.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-pip install --user awscli
+#!/bin/bash/env bash
 
 mkdir -p ~/.aws
 
@@ -9,13 +7,3 @@ cat > ~/.aws/credentials << EOL
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOL
-
-export PATH=$PATH:$HOME/.local/bin
-
-eval $(aws ecr get-login --no-include-email --region us-west-2)
-
-aws ssm get-parameters-by-path --region us-west-2 --with-decryption --path     "/api/affinity/demo/"
- 12   | jq -r '.Parameters[] | ([.Name, "\"" + .Value + "\""]) | join("=")' > /t    mp/dev.affinity.ini
-
-sed -i~ 's#/api/affinity/demo/##' /tmp/dev.affinity.ini
-mv /tmp/dev.affinity.ini .env
